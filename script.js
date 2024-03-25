84f0adb0d3fb6b924c70925dd2f566c37c6096c1
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-03-25 10:01:01 +0700

    commit tambahan

---
+++ script.js
@@ -0,0 +1,11 @@
+Date()
+let tanggal = new Date()
+document.getElementById('tanggal').innerHTML = tanggal;
+
+function tampil(){
+  alert("Ini Adalah Button")
+}
+
+function show(){
+  alert("Ini Adalah pop up untuk tulisan perkenalan")
+}
